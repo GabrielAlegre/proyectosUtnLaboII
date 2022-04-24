d60c28b3d9d999ce7ad7840cffab1ed7822e5202
GabrielAlegre <gabrielalegre68@gmail.com>
2022-04-24 19:35:52 -0300

    Nuevos proyectos de herencia realizados

---
+++ Alegre.Gabriel/Proyectos Herencia/BcHerenciaGoSpeedRacerGo/MotoCross.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BcHerenciaGoSpeedRacerGo
+{
+    public class MotoCross : VehiculoDeCarrera
+    {
+        private string cilindrada;
+
+        public string Cilindrada
+        {
+            get { return this.cilindrada; }
+            set { this.cilindrada = value; }
+        }
+
+        public MotoCross(short numero, string escuderia) : base(numero, escuderia)
+        {
+        }
+
+        public MotoCross(short numero, string escuderia, string cilin) : base(numero, escuderia)
+        {
+            this.cilindrada = cilin;
+        }
+
+
+
+        public string MostrarDatosMotoCross()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.MostrarDatos());
+            sb.AppendLine($"Cilindrada: {this.cilindrada}\n");
+
+
+            return sb.ToString();
+        }
+
+        public static bool operator ==(MotoCross m1, MotoCross m2)
+        {
+            return m1.cilindrada == m2.cilindrada;
+        }
+
+        public static bool operator !=(MotoCross m1, MotoCross m2)
+        {
+            return !(m1 == m2);
+        }
+    }
+}
---
+++ Alegre.Gabriel/Proyectos Herencia/BcHerenciaGoSpeedRacerGo/VehiculoDeCarrera.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BcHerenciaGoSpeedRacerGo
+{
+    public class VehiculoDeCarrera
+    {
+        private short cantidadCombustible;
+        private bool enCompetencia;
+        private string escuderia;
+        private short numero;
+        private short vueltasRestantes;
+
+        public VehiculoDeCarrera(short numero, string escuderia)
+        {
+            this.escuderia = escuderia;
+            this.numero = numero;
+        }
+        public string Escuderia
+        {
+            get { return this.escuderia; }
+            set { this.escuderia = value; }
+        }
+        public short CantidadCombustible
+        {
+            get { return this.cantidadCombustible; }
+            set { this.cantidadCombustible = value; }
+        }
+        public bool EnCompetencia
+        {
+            get { return this.enCompetencia; }
+            set { this.enCompetencia = value; }
+        }
+
+        public short VueltasRestantes
+        {
+            get { return this.vueltasRestantes; }
+            set { this.vueltasRestantes = value; }
+        }
+
+        public short Numero
+        {
+            get { return this.numero; }
+            set { this.numero = value; }
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Cantidad de combustible: {this.cantidadCombustible}");
+            sb.AppendLine($"Esta en competencia: {this.enCompetencia}");
+            sb.AppendLine($"Escuderia: {this.escuderia}");
+            sb.AppendLine($"Numero: {this.numero}");
+            sb.Append($"Vueltas restantes: {this.vueltasRestantes}");
+
+            return sb.ToString();
+        }
+
+        public static bool operator ==(VehiculoDeCarrera v1, VehiculoDeCarrera v2)
+        {
+            return (v1.escuderia == v2.escuderia) && (v1.numero == v2.numero);
+        }
+
+        public static bool operator !=(VehiculoDeCarrera v1, VehiculoDeCarrera v2)
+        {
+            return !(v1 == v2);
+        }
+    }
+}
