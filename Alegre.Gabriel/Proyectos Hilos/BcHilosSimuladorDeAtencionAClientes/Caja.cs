dd31fa8ae1b2e15ded12b5bede412d34d664d240
GabrielAlegre <gabrielalegre68@gmail.com>
2022-06-18 00:41:27 -0300

    Proyectos realizados. Gabriel Alegre.

---
+++ Alegre.Gabriel/Proyectos Hilos/BcHilosSimuladorDeAtencionAClientes/Caja.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+
+namespace BcHilosSimuladorDeAtencionAClientes
+{
+    public class Caja
+    {
+        private static Random atributoRandom;
+        private Queue<string> clientesALaEspera;
+        private string nombreCaja;
+        public delegate void DelegadoClienteAtendido(Caja unaCaja, string unCliente);
+        private DelegadoClienteAtendido delegadoCliente;
+
+        static Caja()
+        {
+            atributoRandom = new Random();
+        }
+
+        public Caja (string nombreDeLaCaja, DelegadoClienteAtendido referenciaMetodo)
+        {
+            this.clientesALaEspera = new Queue<string>();
+            this.nombreCaja = nombreDeLaCaja;
+            this.delegadoCliente = referenciaMetodo;
+        }
+        public string NombreCaja { get => nombreCaja;}
+        public int CantidadDeClientesALaEspera { get => clientesALaEspera.Count;}
+
+        internal  void AgregarCliente(string cliente)
+        {
+            clientesALaEspera.Enqueue(cliente);
+        }
+
+        internal Task IniciarAtencion()
+        {
+            return Task.Run(() =>
+            {
+                do
+                {
+                    if (clientesALaEspera.Any())
+                    {
+                        if (this.delegadoCliente is not null)
+                        {
+                            delegadoCliente.Invoke(this, clientesALaEspera.Dequeue());
+                        }
+
+                        Thread.Sleep(atributoRandom.Next(1000, 5000));
+                    }
+
+                } while (true);
+            });
+
+            /*
+            Task tarea = new Task(tareaAtencionAlCliente);
+            tarea.Start();
+            return tarea;
+            */
+
+        }
+
+        /*
+        private void tareaAtencionAlCliente()
+        {
+            do
+            {
+                if(clientesALaEspera.Any())
+                {
+                    if(this.delegadoCliente is not null)
+                    {
+                        delegadoCliente.Invoke(this, clientesALaEspera.Dequeue());
+                    }
+
+                    Thread.Sleep(atributoRandom.Next(1000,5000));
+                }
+
+            } while (true);
+        }
+        */
+    }
+}
---
+++ Alegre.Gabriel/Proyectos Hilos/BcHilosSimuladorDeAtencionAClientes/Negocio.cs
@@ -0,0 +1,75 @@
+﻿using NameGenerator.Generators;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BcHilosSimuladorDeAtencionAClientes
+{
+    public class Negocio
+    {
+        static RealNameGenerator nameGenerator;
+        private ConcurrentQueue<string> clientes;
+        private List<Caja> cajas;
+        static Negocio()
+        {
+            nameGenerator = new RealNameGenerator();
+        }
+
+        public Negocio(List<Caja> listaDeCaja)
+        {
+            clientes = new ConcurrentQueue<string>();
+            cajas = listaDeCaja;
+
+        }
+
+        public List<Task> ComenzarAtencion()
+        {
+            List<Task> hilos = new List<Task>();
+
+            foreach (Caja unaCaja in cajas)
+            {
+                unaCaja.IniciarAtencion();
+            }
+
+            hilos.Add(Task.Run(tareaSimulacionClientes));
+            hilos.Add(Task.Run(tareaAsignacionCajas));
+
+
+            return hilos;
+        }
+
+        private void tareaSimulacionClientes()
+        {
+            do
+            {
+                clientes.Enqueue(nameGenerator.Generate());
+                Thread.Sleep(1000);
+
+            } while (true);
+        }
+
+        private void tareaAsignacionCajas()
+        {
+            do
+            {
+                //Primero ordeno de las cajas que menos clientes tienen hasta la que mas clientes tienen,
+                //Despues agarro (con el first) la primera caja de la lista, que esta primera caja va a ser la que menos clientes tenga
+                Caja cajaConMenosClientes = cajas.OrderBy(c => c.CantidadDeClientesALaEspera).First();
+
+                //De mi lista de clientes agarro el proximo y lo mando a la caja que menos clientes tenga
+                clientes.TryDequeue(out string cliente);
+
+                if (!string.IsNullOrWhiteSpace(cliente))
+                {
+                    cajaConMenosClientes.AgregarCliente(cliente);
+                }
+
+            } while (true);
+        }
+
+    }
+}
